571c82da815e82e9794d981e4d977771de15efa0
hehuifang <11111111@qq.com>
2018-06-19 09:14:55 +0800

    “第一遍”

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+var gulp = require('gulp');
+var server = require('gulp-webserver');
+var sass = require('gulp-sass');
+var Css = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
+var ug = uglify('gulp-uglify');
---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+require.config({
+    baseUrl: 'js/',
+    paths: {
+        index: 'lib/index',
+        jquery: 'lib/jquery',
+        handlebars: 'lib/handlebars-v4.0.11'
+    }
+})
